8ace6754c92b17e6f86b42b68ab350132fd76bf0
cesar <cesar@usemobile.xyz>
2019-04-30 18:42:56 -0300

    start calculating possible movements

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -41,11 +41,34 @@ class Board extends Component {
     this.state = { ...this.state, pieces }
   }
 
+  getMovements = (i0, j0) => {
+    let positions = new Array(DIMENSION)
+    for (let i = 0; i < DIMENSION; i++) {
+      positions[i] = new Array(DIMENSION)
+      for (let j = 0; j < DIMENSION; j++) {
+        positions[i][j] = false
+      }
+    }
+
+    if (this.state.pieces[i0][j0] === TYPES.BISHOP) {
+      const diagonal = j0 - i0
+      for (let j = 0; j < DIMENSION; j++) {
+        const i1 = j - j0 + i0
+        const i2 = i0 + j0 - j
+        if (i1 >= 0 && i1 < DIMENSION) positions[i1][j] = true
+        if (i2 >= 0 && i2 < DIMENSION) positions[i2][j] = true
+      }
+    }
+
+    return positions
+  }
+
   render () {
+    const movements = this.getMovements(3,3)
     return (<table className={classes.Board}>
       <tbody>
         {[...Array(DIMENSION).keys()].map(i => <tr key={i}>
-          {[...Array(DIMENSION).keys()].map(j => <td key={j}>
+          {[...Array(DIMENSION).keys()].map(j => <td key={j} className={movements[i][j]? classes.active: null}>
             {i === DIMENSION - 1? <span className={[classes.Info, classes.Column].join(' ')}>{String.fromCharCode(65 + j)}</span>: null}
             {j === 0? <span className={[classes.Info, classes.Row].join(' ')}>{i + 1}</span>: null}
             {this.state.pieces[i][j]? <Piece type={this.state.pieces[i][j].type} color={this.state.pieces[i][j].color}/>: null}
