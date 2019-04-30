813164ab54e4a36cfcae7dffe9c2cf0accaad544
cesar <cesar@usemobile.xyz>
2019-04-30 11:23:14 -0300

    filling board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -15,23 +15,41 @@ class Board extends Component {
     for (let i = 0; i < DIMENSION; i++) {
       pieces[i] = new Array(DIMENSION)
       for (let j = 0; j < DIMENSION; j++) {
-        pieces[i][j] = false
+        if (i >= 2 && i <= 5) pieces[i][j] = null
+        else {
+          let piece = {
+            color: (i < 2)? COLORS.BLACK: COLORS.WHITE,
+            type: TYPES.PAWN
+          }
+          if (i === 0 || i === 7) {
+            switch (j) {
+              case 0: piece.type = TYPES.ROOK; break
+              case 1: piece.type = TYPES.KNIGHT; break
+              case 2: piece.type = TYPES.BISHOP; break
+              case 3: piece.type = TYPES.QUEEN; break
+              case 4: piece.type = TYPES.KING; break
+              case 5: piece.type = TYPES.BISHOP; break
+              case 6: piece.type = TYPES.KNIGHT; break
+              case 7: piece.type = TYPES.ROOK; break
+              default: break
+            }
+          }
+          pieces[i][j] = piece
+        }
       }
     }
-    pieces[0][0] = true
     this.state = { ...this.state, pieces }
   }
 
   render () {
-    console.log(this.state)
     return (<table className={classes.Board}>
       <tbody>
         {[...Array(DIMENSION).keys()].map(i => <tr key={i}>
           {[...Array(DIMENSION).keys()].map(j => <td key={j}>
             {i === DIMENSION - 1? <span className={[classes.Info, classes.Column].join(' ')}>{String.fromCharCode(65 + j)}</span>: null}
             {j === 0? <span className={[classes.Info, classes.Row].join(' ')}>{i + 1}</span>: null}
-            {this.state.pieces[i][j]? <Piece type={TYPES.BISHOP} color={COLORS.WHITE}/>: null}
-          </td> )}
+            {this.state.pieces[i][j]? <Piece type={this.state.pieces[i][j].type} color={this.state.pieces[i][j].color}/>: null}
+          </td>)}
         </tr>)}
       </tbody>
     </table>)
